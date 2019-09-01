ddc391e53743d38c347eb3895f9590fc8e288e7d
Swapnil Hedau <swapnilhedau@hotmail.com>
2019-09-01 01:08:46 -0400

    added api urls in env files.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'http://localhost:8081',
+  apiUserPrefix: '/user/',
+  apiProjectPrefix: '/project/',
+  apiTaskPrefix: '/task/'
 };

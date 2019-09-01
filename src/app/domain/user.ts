c46006ab2cdcc3a4caefc08f1aa1dbdea38d0687
Swapnil Hedau <swapnilhedau@hotmail.com>
2019-09-01 01:10:04 -0400

    added user class and spec

---
+++ src/app/domain/user.ts
@@ -0,0 +1,10 @@
+export class User {
+  constructor(
+    public userId: number = 0,
+    public firstName: string = '',
+    public lastName: string = '',
+    public employeeId: number = 0,
+    public projectId: number = 0,
+    public taskId: number = 0
+    ) { }
+  }

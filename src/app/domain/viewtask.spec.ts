547bc5a3a55e4430843a694967362ec4f315b242
Swapnil Hedau <swapnilhedau@hotmail.com>
2019-09-06 02:25:20 -0400

    Fixed specs.

--- src/app/domain/viewtask.spec.ts
+++ src/app/domain/viewtask.spec.ts
@@ -1,7 +1,9 @@
 import { Viewtask } from './viewtask';
+import { Task } from './task';
+import { Parenttask } from './parenttask';
 
 describe('Viewtask', () => {
   it('should create an instance', () => {
-    expect(new Viewtask()).toBeTruthy();
+    expect(new Viewtask(new Task(), new Parenttask())).toBeTruthy();
   });
 });
--- src/app/service/viewtask.service.spec.ts
+++ src/app/service/viewtask.service.spec.ts
@@ -1,9 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ViewtaskService } from './viewtask.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('ViewtaskService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientModule]
+  }));
 
   it('should be created', () => {
     const service: ViewtaskService = TestBed.get(ViewtaskService);
